355da12611610b1ffc373a5ba94bdbf785aa8535
liyu-wang <liyu.wang@oasis.com>
2019-03-09 09:00:22 +1100

    new project created.

---
+++ Weathers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weathers
+//
+//  Created by Liyu Wang on 9/3/19.
+//  Copyright © 2019 Liyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
